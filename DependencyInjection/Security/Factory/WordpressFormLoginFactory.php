4e7896d736f09bbd90ae994cc45e03c23b3dad4c
Miquel Rodríguez Telep / Michael Rodríguez-Torrent <miquel@designunbound.co.uk>
2011-10-08 18:35:24 +0100

    Factory for Wordpress login authentication

---
+++ DependencyInjection/Security/Factory/WordpressFormLoginFactory.php
@@ -0,0 +1,23 @@
+<?php
+namespace Hypebeast\WordpressBundle\DependencyInjection\Security\Factory;
+
+use Symfony\Bundle\SecurityBundle\DependencyInjection\Security\Factory\FormLoginFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
+
+class WordpressFormLoginFactory extends FormLoginFactory
+{
+    protected function createAuthProvider(ContainerBuilder $container, $id, $config, $userProviderId)
+    {
+        $providerId = 'security.authentication.provider.wordpress.login.' . $id;
+        $container->setDefinition($providerId,
+                    new DefinitionDecorator('wordpress.security.authentication.provider.login'));
+
+        return $providerId;
+    }
+
+    public function getKey()
+    {
+        return 'wordpress-form-login';
+    }
+}
