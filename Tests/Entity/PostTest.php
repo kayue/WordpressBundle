79ef5c92763480a197be8ea404bd57b8b0f848f9
Ka Yue Yeung <kayuey@gmail.com>
2012-06-05 08:21:26 +0800

    Add unit test.

--- Tests/Entity/PostTest.php
+++ Tests/Entity/PostTest.php
@@ -32,34 +32,76 @@ class PostTest extends WebTestCase
         parent::tearDown();
     }
 
-    public function testFindAll()
+    /**
+     * @dataProvider postProvider
+     */
+    public function testNewPost($title, $content, $userId)
     {
-        $postRepository = $this->em->getRepository('HypebeastWordpressBundle:Post');
-        $userRepository = $this->em->getRepository('HypebeastWordpressBundle:User');
-        
-        $count = count($postRepository->findAll());
-
         $post = new Post();
+        $post->setPostTitle($title);
         $post->setPostName('Test');
-        $post->setPostContent('Content');
-        $post->setPostTitle('Title');
+        $post->setPostContent($content);
         $post->setPostExcerpt('setPostExcerpt');
-        $post->setUser($userRepository->find(1));
+        $post->setUser($this->getUserRepository()->find($userId));
 
         $this->em->persist($post);
         $this->em->flush();
 
-        // get the newly added post for tests
-        $post = $postRepository->find($post->getId());
+        $result = $this->getPostRepository()
+            ->createQueryBuilder('post')
+            ->orderBy('post.ID', 'DESC')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getSingleResult();
 
-        $this->assertCount($count + 1, $postRepository->findAll());
-        $this->assertEquals('Title', $post->getPostTitle());
+        $this->assertEquals($title, $result->getPostTitle());
+        $this->assertEquals($content, $result->getPostContent());
+        $this->assertEquals($userId, $result->getUser()->getId());
+    }
 
-        // test update        
-        $post->setPostTitle('New Title');
-        $this->em->flush();
+    /**
+     * @depends testNewPost
+     */
+    public function testUpdatePost($post)
+    {
+        echo $post->getPostTitle();
 
-        $post = $postRepository->find($post->getId());
-        $this->assertEquals('New Title', $post->getPostTitle());
+        $this->assertTrue(1);
+    }
+
+    public function postProvider()
+    {
+        return array(
+            array('Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 1),
+            array('Sed ut perspiciatis unde', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.', 1)
+        );
+    }
+
+    // public function testFindAll()
+    // {
+    //     $count = count($this->getPostRepository()->findAll());
+
+    //     // get the newly added post for tests
+    //     $post = $postRepository->find($post->getId());
+
+    //     $this->assertCount($count + 1, $postRepository->findAll());
+    //     $this->assertEquals('Title', $post->getPostTitle());
+
+    //     // test update        
+    //     $post->setPostTitle('New Title');
+    //     $this->em->flush();
+
+    //     $post = $postRepository->find($post->getId());
+    //     $this->assertEquals('New Title', $post->getPostTitle());
+    // }
+
+    protected function getPostRepository()
+    {
+        return $this->em->getRepository('HypebeastWordpressBundle:Post');
+    }
+
+    protected function getUserRepository()
+    {
+        return $this->em->getRepository('HypebeastWordpressBundle:User');
     }
 }
