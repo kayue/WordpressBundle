3cc47f1b603be4a3fb75e295a35cb487b7594f9b
Ka Yue Yeung <kayuey@gmail.com>
2012-07-07 16:13:40 +0800

    Twig: Add Twig wpautop filter.

--- Extensions/WordpressTwigExtension.php
+++ Extensions/WordpressTwigExtension.php
@@ -8,21 +8,44 @@ namespace Hypebeast\WordpressBundle\Extensions;
  *
  * @author Ka-Yue Yeung <kayuey@gmail.com>
  */
-class WordpressTwigExtension extends \Twig_Extension 
+class WordpressTwigExtension extends \Twig_Extension
 {
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
     public function getName()
     {
         return 'wordpress';
     }
 
+    /**
+     * Returns a list of global variables to add to the existing list.
+     *
+     * @return array An array of global variables
+     */
     public function getGlobals()
     {
         return array(
-            'wp' => $this
+            'wp' => &$this
         );
     }
 
-    public function __call($function, $arguments) {
+    /**
+     * Returns a list of filters to add to the existing list.
+     *
+     * @return array An array of filters
+     */
+    public function getFilters()
+    {
+        return array(
+            'wpautop' => new \Twig_Filter_Method(&$this, 'wpautop'),
+        );
+    }
+
+    public function __call($function, $arguments)
+    {
         $function = $this->camelcaseToUnderscore($function);
 
         // Since a lot of Wordpress plugins are poorly written, we recommend you
@@ -30,12 +53,109 @@ class WordpressTwigExtension extends \Twig_Extension
         return call_user_func_array($function, $arguments);
     }
 
-    /** 
+    /**
      * Convert function name from camolcasing to underscoring.
      *
      * @param String $name The function name.
      */
-    private function camelcaseToUnderscore($name) {
+    private function camelcaseToUnderscore($name)
+    {
         return preg_replace('/([a-z])([A-Z])/', '$1_$2', $name);
+    }
+
+    /**
+     * Replaces double line-breaks with paragraph elements.
+     *
+     * A group of regex replaces used to identify text formatted with newlines and
+     * replace double line-breaks with HTML paragraph tags. The remaining
+     * line-breaks after conversion become <<br />> tags, unless $br is set to '0'
+     * or 'false'.
+     *
+     * @param string $pee The text which has to be formatted.
+     * @param bool $br Optional. If set, this will convert all remaining line-breaks after paragraphing. Default true.
+     * @return string Text which has been converted into correct paragraph tags.
+     */
+    public function wpautop($pee, $br = true)
+    {
+        $pre_tags = array();
+
+        if ( trim($pee) === '' )
+            return '';
+
+        $pee = $pee . "\n"; // just to make things a little easier, pad the end
+
+        if ( strpos($pee, '<pre') !== false ) {
+            $pee_parts = explode( '</pre>', $pee );
+            $last_pee = array_pop($pee_parts);
+            $pee = '';
+            $i = 0;
+
+            foreach ( $pee_parts as $pee_part ) {
+                $start = strpos($pee_part, '<pre');
+
+                // Malformed html?
+                if ( $start === false ) {
+                    $pee .= $pee_part;
+                    continue;
+                }
+
+                $name = "<pre wp-pre-tag-$i></pre>";
+                $pre_tags[$name] = substr( $pee_part, $start ) . '</pre>';
+
+                $pee .= substr( $pee_part, 0, $start ) . $name;
+                $i++;
+            }
+
+            $pee .= $last_pee;
+        }
+
+        $pee = preg_replace('|<br />\s*<br />|', "\n\n", $pee);
+        // Space things out a little
+        $allblocks = '(?:table|thead|tfoot|caption|col|colgroup|tbody|tr|td|th|div|dl|dd|dt|ul|ol|li|pre|select|option|form|map|area|blockquote|address|math|style|p|h[1-6]|hr|fieldset|legend|section|article|aside|hgroup|header|footer|nav|figure|figcaption|details|menu|summary)';
+        $pee = preg_replace('!(<' . $allblocks . '[^>]*>)!', "\n$1", $pee);
+        $pee = preg_replace('!(</' . $allblocks . '>)!', "$1\n\n", $pee);
+        $pee = str_replace(array("\r\n", "\r"), "\n", $pee); // cross-platform newlines
+        if ( strpos($pee, '<object') !== false ) {
+            $pee = preg_replace('|\s*<param([^>]*)>\s*|', "<param$1>", $pee); // no pee inside object/embed
+            $pee = preg_replace('|\s*</embed>\s*|', '</embed>', $pee);
+        }
+        $pee = preg_replace("/\n\n+/", "\n\n", $pee); // take care of duplicates
+        // make paragraphs, including one at the end
+        $pees = preg_split('/\n\s*\n/', $pee, -1, PREG_SPLIT_NO_EMPTY);
+        $pee = '';
+        foreach ( $pees as $tinkle )
+            $pee .= '<p>' . trim($tinkle, "\n") . "</p>\n";
+        $pee = preg_replace('|<p>\s*</p>|', '', $pee); // under certain strange conditions it could create a P of entirely whitespace
+        $pee = preg_replace('!<p>([^<]+)</(div|address|form)>!', "<p>$1</p></$2>", $pee);
+        $pee = preg_replace('!<p>\s*(</?' . $allblocks . '[^>]*>)\s*</p>!', "$1", $pee); // don't pee all over a tag
+        $pee = preg_replace("|<p>(<li.+?)</p>|", "$1", $pee); // problem with nested lists
+        $pee = preg_replace('|<p><blockquote([^>]*)>|i', "<blockquote$1><p>", $pee);
+        $pee = str_replace('</blockquote></p>', '</p></blockquote>', $pee);
+        $pee = preg_replace('!<p>\s*(</?' . $allblocks . '[^>]*>)!', "$1", $pee);
+        $pee = preg_replace('!(</?' . $allblocks . '[^>]*>)\s*</p>!', "$1", $pee);
+        if ( $br ) {
+            $pee = preg_replace_callback('/<(script|style).*?<\/\\1>/s', array(&$this, 'autopNewlinePreservationHelper'), $pee);
+            $pee = preg_replace('|(?<!<br />)\s*\n|', "<br />\n", $pee); // optionally make line breaks
+            $pee = str_replace('<WPPreserveNewline />', "\n", $pee);
+        }
+        $pee = preg_replace('!(</?' . $allblocks . '[^>]*>)\s*<br />!', "$1", $pee);
+        $pee = preg_replace('!<br />(\s*</?(?:p|li|div|dl|dd|dt|th|pre|td|ul|ol)[^>]*>)!', '$1', $pee);
+        $pee = preg_replace( "|\n</p>$|", '</p>', $pee );
+
+        if ( !empty($pre_tags) )
+            $pee = str_replace(array_keys($pre_tags), array_values($pre_tags), $pee);
+
+        return $pee;
+    }
+
+    /**
+     * Newline preservation help function for wpautop
+     *
+     * @param array $matches preg_replace_callback matches array
+     * @return string
+     */
+    private function autopNewlinePreservationHelper($matches)
+    {
+        return str_replace("\n", "<WPPreserveNewline />", $matches[0]);
     }
 }
